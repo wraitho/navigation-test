e3118efcc8349f047b56651d8b990a91c009217d
Felipe Pedroso <felipe-pedroso@cookpad.com>
2019-09-04 15:11:31 +0100

    Implementing the items screen.

--- app/src/main/java/br/pedroso/navigationtest/details/DetailsFragment.kt
+++ app/src/main/java/br/pedroso/navigationtest/details/DetailsFragment.kt
@@ -5,11 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import br.pedroso.navigationtest.R
+import kotlinx.android.synthetic.main.fragment_details.view.*
 
 class DetailsFragment : Fragment() {
 
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val args: DetailsFragmentArgs by navArgs()
+
+
+        args.item?.run {
+            view.titleTextView.text = title
+            view.descriptionTextView.text = description
+        }
     }
 }
