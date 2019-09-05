ba3da31f84fbe256b6daf98b6b1b9a14c607eec2
Felipe Pedroso <felipe-pedroso@cookpad.com>
2019-09-05 11:26:27 +0100

    Implementing an implicit deeplink.

--- app/src/main/java/br/pedroso/navigationtest/bookmarks/BookmarksFragment.kt
+++ app/src/main/java/br/pedroso/navigationtest/bookmarks/BookmarksFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import br.pedroso.navigationtest.R
@@ -18,10 +19,29 @@ class BookmarksFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        arguments?.run {
+            if (containsKey(KEY_REGION_CODE)) {
+                Toast.makeText(
+                    requireContext(),
+                    getString(KEY_REGION_CODE, REGION_NONE),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        if(arguments?.containsKey(KEY_REGION_CODE) == true) {
+
+        }
+
         SetupSharedToolbarDelegate.setupSearchQueryEditText(
             findNavController(),
             resources,
             searchQueryEditText
         )
+    }
+
+    companion object {
+        private const val KEY_REGION_CODE = "region_code"
+        private const val REGION_NONE = "none"
     }
 }
